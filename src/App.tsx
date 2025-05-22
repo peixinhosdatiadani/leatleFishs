e70f7776dcdc47180bfb9d3b474f842a991a7275
Ricardo Rodrigo de Oliveira <47369938+ricardorodrigooliveira@users.noreply.github.com>
2025-05-22 14:58:50 -0300

    first_commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import leatleFishsLogo from './assets/logoPeixinhos.jpg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a>
+          <img src={leatleFishsLogo} className="logo" alt="Leatle Fishs" />
+        </a>
+      </div>
+      <h1>Pagina em contrução</h1>
+    </>
+  )
+}
+
+export default App
